7aef0ef04e0414d45f0e7ab65ed8444fdc91036d
dradnats1012 <herjebi1012@gmail.com>
2024-11-22 10:30:38 +0900

    feat: 코치 Name 클래스 구현

---
+++ src/main/java/menu/model/coach/Name.java
@@ -0,0 +1,19 @@
+package menu.model.coach;
+
+import static menu.util.ErrorMessage.ERROR_NAME_LENGTH;
+
+public class Name {
+
+    private final String name;
+
+    public Name(String name){
+        validateNameLength(name);
+        this.name = name;
+    }
+
+    private void validateNameLength(String name){
+        if(name.length() < 2 || name.length() > 4){
+            throw new IllegalArgumentException(ERROR_NAME_LENGTH.getMessage());
+        }
+    }
+}
