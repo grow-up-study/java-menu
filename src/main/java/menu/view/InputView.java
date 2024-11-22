7b082a654edc64c424db0e20ca9cff8314be72cd
dradnats1012 <herjebi1012@gmail.com>
2024-11-22 10:36:21 +0900

    feat: InputView 클래스 구현

---
+++ src/main/java/menu/view/InputView.java
@@ -0,0 +1,19 @@
+package menu.view;
+
+import static menu.util.Message.INPUT_COACH_NAME;
+import static menu.util.Message.INPUT_HATE_FOOD;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static String getCoachNames() {
+        System.out.println(INPUT_COACH_NAME.getMessage());
+        return Console.readLine();
+    }
+
+    public static String getHateFoods(String name){
+        System.out.printf(INPUT_HATE_FOOD.getMessage(), name);
+        return Console.readLine();
+    }
+}
