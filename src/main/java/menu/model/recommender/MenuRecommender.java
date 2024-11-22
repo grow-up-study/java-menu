87b5aa7dc3d6f59abd1d059058953abdeec8e9bf
HyeonsuLee <leehyeonsu4888@naver.com>
2024-11-22 13:31:54 +0900

    feat: 요일별 메뉴를 한번에 추천하는 함수 작성

--- src/main/java/menu/model/recommender/MenuRecommender.java
+++ src/main/java/menu/model/recommender/MenuRecommender.java
@@ -1,10 +1,20 @@
 package menu.model.recommender;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import java.util.stream.Collectors;
 import menu.model.Coach;
+import menu.model.DaysCategory;
 import menu.model.MenuCategory;
 
 public class MenuRecommender {
+
+    public List<String> recommendMenus(Coach coach, DaysCategory daysCategory) {
+        return daysCategory.getCategories().values()
+                .stream()
+                .map(menuCategory -> recommendMenu(coach, menuCategory))
+                .collect(Collectors.toList());
+    }
 
     public String recommendMenu(Coach coach, MenuCategory menuCategory) {
         String menu = pickRandomMenu(menuCategory);
