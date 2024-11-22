62712e2d0f1167c5b99c256e6758825403ed330e
dradnats1012 <herjebi1012@gmail.com>
2024-11-22 14:16:26 +0900

    feat: HateFood 클래스 getName 검증 로직 추가

--- src/main/java/menu/model/food/HateFoods.java
+++ src/main/java/menu/model/food/HateFoods.java
@@ -1,14 +1,16 @@
 package menu.model.food;
 
 import static menu.util.message.ErrorMessage.ERROR_DUPLICATE_MENUS;
+import static menu.util.message.ErrorMessage.ERROR_HATE_FOODS_COUNT;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import menu.util.parser.MenuParser;
 
 public class HateFoods {
 
-    private List<Food> foods;
+    private final List<Food> foods = new ArrayList<>();
 
     public HateFoods(String inputfoods) {
         splitFoods(inputfoods);
@@ -26,6 +28,9 @@ public class HateFoods {
     private void validateFoodsCount() {
         if (foods.size() != foods.stream().distinct().count()) {
             throw new IllegalArgumentException(ERROR_DUPLICATE_MENUS.getMessage());
+        }
+        if(foods.size() > 2){
+            throw new IllegalArgumentException(ERROR_HATE_FOODS_COUNT.getMessage());
         }
     }
 
